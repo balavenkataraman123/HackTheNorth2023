068de31ce0200ba689ba04b2209fd376103b41df
Daniel Ye <danielyeqt@gmail.com>
2023-09-16 15:48:45 -0400

    Add private & document key storage

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,4 +1,26 @@
 import {Agent} from '@dfinity/agent'
 import {createActor} from './declarations/backend'
+import * as crypto from 'crypto-js'
 
 const agent = createActor("be2us-64aaa-aaaaa-qaabq-cai", {actorOptions: {host: 'localhost'}})
+
+export const getKey = () => {
+    if(localStorage.getItem('private_key') === null) {
+        localStorage.setItem('private_key', crypto.lib.WordArray.random(32))
+    }
+    return localStorage.getItem('private_key')
+}
+
+export const getImageKeys = () => {
+    if(localStorage.getItem('documents') === null) {
+        localStorage.setItem('documents', JSON.stringify([]))
+    }
+    return JSON.parse(localStorage.getItem('documents'))
+}
+
+export const addImageKey = (imageKey) => {
+    const imageKeys = getImageKeys()
+    imageKeys.push(imageKey)
+    localStorage.setItem('documents', JSON.stringify(imageKeys))
+    return imageKeys
+}
